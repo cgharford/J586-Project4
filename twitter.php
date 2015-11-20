87437ed3195c258af02cec86420d6671e9d41b00
cgharford <charford@live.unc.edu>
2015-11-20 08:30:09 -0500

    Added tweetLinkIt

--- twitter.php
+++ twitter.php
@@ -1,6 +1,13 @@
 <html>
 <head>
     <link rel="stylesheet" href="css/styles.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="js/tweetLinkIt.js"></script>
+    <script>
+        function pageComplete(){
+            $('.tweet').tweetLinkify();
+        }
+    </script>
 </head>
 <body>
     <?php
@@ -15,47 +22,32 @@
         'consumer_secret' => "kXibOHdLGWFpdOgN2eQzfEmzWublk1Rf9NoreEx4qUSap07CF0"
     );
 
-    /** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
-    // $url = 'https://api.twitter.com/1.1/search/tweets.json';
-    // $requestMethod = 'POST';
-    //
-    // /** POST fields required by the URL above. See relevant docs as above **/
-    // $postfields = array(
-    //     'screen_name' => 'usernameToBlock',
-    //     'skip_status' => '1'
-    // );
-    //
-    // /** Perform a POST request and echo the response **/
-    // $twitter = new TwitterAPIExchange($settings);
-    // echo $twitter->buildOauth($url, $requestMethod)
-    //              ->setPostfields($postfields)
-    //              ->performRequest();
-
     /** Perform a GET request and echo the response **/
     /** Note: Set the GET field BEFORE calling buildOauth(); **/
     $url = 'https://api.twitter.com/1.1/search/tweets.json';
-    $getfield = '?q=unc';
+    //$domDocumentObject->getElementById('search-twitter');
+    $keyword = '#election2016';
+    $getfield = '?q=' . $keyword;
     $requestMethod = 'GET';
     $twitter = new TwitterAPIExchange($settings);
-    // echo $twitter->setGetfield($getfield)
-    //              ->buildOauth($url, $requestMethod)
-    //              ->performRequest();
 
     $tweetData = json_decode($twitter->setGetfield($getfield)
                 ->buildOauth($url, $requestMethod)
                 ->performRequest(),$assoc = TRUE);
-
-    // echo $tweetData;
 
     foreach($tweetData['statuses'] as $items){
         echo "<div class='tweet-container'>";
         echo "<img class='twitter-profile-picture' src='" . $items['user']['profile_image_url'] . "'>";
         echo "<div class='twitter-user'>";
         echo "<div class='twitter-username'>" . $items['user']['name'] . "<span class='twitter-screen-name'> @" . $items['user']['screen_name'] .  "</span></div>";
-        echo "<div class='tweet-content'>" . $items['text'] . "</div>";
+        echo "<div class='tweet-content tweet'>" . $items['text'] . "</div>";
+        $tweetPhotoUrl = $items['entities']['media']['url'];
+        echo $tweetPhotoUrl;
+        echo "<img class='tweet-photo' src='" . $items['entities']['media']['url'] . "'>" . $items['entities']['media']['url'];
         echo "</div>";
         echo "</div>";
     }
+    echo "<script>pageComplete();</script>";
     ?>
 </body>
 </html>
